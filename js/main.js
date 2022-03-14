67fa31993018a0410b284e8382d1199259697a45
flor <giubileiflor@gmail.com>
2022-03-14 15:52:08 -0300

    Arrays

--- js/main.js
+++ js/main.js
@@ -9,3 +9,23 @@ for (let i = 0; i < 1; i++){
             document.write(`No tienes productos en el carrito ${productosSeleccionados[i]}`);
         }
 }
+const listaCursos = [];
+let cantidad = 6;
+
+do{
+    let entrada = prompt ("Ingresa el curso");
+    listaCursos.push(entrada.toUpperCase());
+    console.log(listaCursos.length);
+}while(listaCursos.length != cantidad)
+
+const cursos = [{ id: 1, producto: "Pan Masa madre" },
+                {id: 2, producto: "Curso Tortas"},
+                {id: 3, producto: "Curso Budines"},
+                {id: 4, producto: "Curso Postres"},
+                {id: 5, producto: "Curso Postres 2"},
+                {id: 6, producto: "Curso Muffins"}];
+
+for (const producto of cursos){
+    console.log(producto.id);
+    console.log(producto.producto);
+}
